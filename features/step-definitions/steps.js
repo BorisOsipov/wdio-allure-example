454790e5dbb7b287c97df89bbdde541f686f7419
Boris Osipov <osipov.boris@gmail.com>
2018-02-28 19:31:27 +0300

    Init
    Signed-off-by: Boris Osipov <osipov.boris@gmail.com>

---
+++ features/step-definitions/steps.js
@@ -0,0 +1,16 @@
+import {defineSupportCode} from 'cucumber'
+
+export default defineSupportCode(function ({Given, When, Then}) {
+    When('I create a user:', (url) => {
+
+    });
+    Then('the Edit User page validation elements are confirmed', (url) => {
+
+    });
+
+
+    Then('I confirm the user has been created successfully', () => {
+
+    });
+
+})
